1d4eec8c5f7cc9d0ae2a229ba4f30079fa53cb7f
Abhijithabhi <abhiabhijith367@gmail.com>
2021-01-21 20:36:28 +0530

    Update Cramers_rule.m

--- Cramers_rule.m
+++ Cramers_rule.m
@@ -4,12 +4,12 @@ a=input('enter row 1 of matrix');
 b=input('enter row 2 of matrix');
 c=input('enter row 3 of matrix');
 d=input('enter RHS');
-A = [a d(1,1); b d(1,2); c d(1,3)];  % Coefficient Matrix
-B = [a; b ; c ];
-C = det(B);
-c1 = [A(1,4) A(1,2) A(1,3); A(2,4) A(2,2) A(2,3); A(3,4) A(3,2) A(3,3); ];
+A = [a d(1,1); b d(1,2); c d(1,3)];        
+B = [a; b ; c ];                   % Coefficients of equations packed into Matrix B
+C = det(B);                        % Determinant of B
+c1 = [A(1,4) A(1,2) A(1,3); A(2,4) A(2,2) A(2,3); A(3,4) A(3,2) A(3,3); ]; % Replacing Corresponding coloumns with RHS
 v1 = det(c1);
-V1 = (v1/C);
+V1 = (v1/C);                      
 
 c2 = [A(1,1) A(1,4) A(1,3); A(2,1) A(2,4) A(2,3); A(3,1) A(3,4) A(3,3); ];
 v2 = det(c2);
