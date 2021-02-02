c3d3c31e49ab9b5ad4bfcf5b9519ceef17f9d7c6
Abhijithabhi <abhiabhijith367@gmail.com>
2021-02-02 21:57:28 +0530

    Update and rename Matrix_Calculator.m to Convolution_Calculator.m

---
+++ Convolution_Calculator.m
@@ -0,0 +1,6 @@
+clear 
+A = input('Enter y(n) in the format [x y z]...  \n > ');
+                                  % Enter matrix A
+B = input('Enter h(n) in the format [x ;y ;z]... \n >'); 
+                                  % Enter matrix B
+X = 
