7e8f02e3aa7e71e616a9fb6c120f808db78ad5c6
Abhijithabhi <abhiabhijith367@gmail.com>
2021-02-02 22:18:21 +0530

    Update Convolution_Calculator.m

--- Convolution_Calculator.m
+++ Convolution_Calculator.m
@@ -1,6 +1,9 @@
 clear 
-A = input('Enter y(n) in the format [x y z]...  \n > ');
+A = input('Enter y(n) in the format [u v w x y z]...  \n > ');
                                   % Enter matrix A
-B = input('Enter h(n) in the format [x ;y ;z]... \n >'); 
+B = input('Enter h(n) in the format [u; v; w; x ;y ;z]... \n >'); 
                                   % Enter matrix B
-X = 
+X = [A(1,1) A(1,6) A(1,5) A(1,4) A(1,3) A(1,2) ; A(1,2) A(1,1) A(1,6) A(1,5) A(1,4) A(1,3); A(1,3) A(1,2) A(1,1) A(1,6) A(1,5) A(1,4); A(1,4) A(1,3) A(1,2) A(1,1) A(1,6) A(1,5); A(1,5) A(1,4) A(1,3) A(1,2) A(1,1) A(1,6); A(1,6) A(1,5) A(1,4) A(1,3) A(1,2) A(1,1)]; 
+
+Y = B*X ;
+disp(Y)
