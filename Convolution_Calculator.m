0385cb259ddd240e00eeaacae2b609f1dbed8c82
Abhijithabhi <abhiabhijith367@gmail.com>
2021-02-03 18:40:24 +0530

    Update Convolution_Calculator.m

--- Convolution_Calculator.m
+++ Convolution_Calculator.m
@@ -1,9 +1,9 @@
 clear 
 A = input('Enter y(n) in the format [u v w x y z]...  \n > ');
-                                  % Enter matrix A
+                                  % Enter matrix y(n)
 B = input('Enter h(n) in the format [u; v; w; x ;y ;z]... \n >'); 
-                                  % Enter matrix B
+                                  % Enter matrix h(n)
 X = [A(1,1) A(1,6) A(1,5) A(1,4) A(1,3) A(1,2) ; A(1,2) A(1,1) A(1,6) A(1,5) A(1,4) A(1,3); A(1,3) A(1,2) A(1,1) A(1,6) A(1,5) A(1,4); A(1,4) A(1,3) A(1,2) A(1,1) A(1,6) A(1,5); A(1,5) A(1,4) A(1,3) A(1,2) A(1,1) A(1,6); A(1,6) A(1,5) A(1,4) A(1,3) A(1,2) A(1,1)]; 
-
-Y = B*X ;
-disp(Y)
+                                  % y(n) converted to a Square matrix
+Y = X*B ;                         % Calculates Circular convolution
+disp(Y)                           
